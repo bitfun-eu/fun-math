28fa9e0bbd3f5b7b0d62a95ffe0ce0aed785133f
Hua Yanghao <yanghao@users.noreply.github.com>
2021-05-08 11:19:06 +0200

    Update add.py

--- add.py
+++ add.py
@@ -17,5 +17,13 @@ def mult(x, y):
         r = add(r, x)
     return r
 
+def factorial(n):
+    r = 1
+    for i in range(n): # range(n): 0, 1, 2,...,n-1.
+        r = r*add_one(i)
+    return r
+
 print("add(3,4) = ", add(3,4))
 print("mult(4, 5) = ", mult(4,5))
+print("factorial(3) = ", factorial(3))
+print("factorial(5) = ", factorial(5))
